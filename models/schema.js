4fd07934202576aa8db0f454e910468dcf9f5fdc
Vaishnav N Mangalam <44635792+vaishnavnamboodirim@users.noreply.github.com>
2022-02-04 02:30:54 +0530

    Add files via upload

--- models/schema.js
+++ models/schema.js
@@ -1,1 +1,12 @@
+const mongoose = require ('mongoose')
 
+
+const tutorialSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    published: Boolean
+},
+{timestamps: true}
+)
+
+module.exports = mongoose.model('Tutorial', tutorialSchema)
