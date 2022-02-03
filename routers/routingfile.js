e6da1dee78959753b1dd47578acf134a5b278ac9
Vaishnav N Mangalam <44635792+vaishnavnamboodirim@users.noreply.github.com>
2022-02-04 02:32:23 +0530

    Add files via upload

--- routers/routingfile.js
+++ routers/routingfile.js
@@ -1,1 +1,145 @@
+const express = require('express');
+const { status } = require('express/lib/response');
 
+const router = express.Router()
+const Tutorial = require('../models/schema')
+
+//new tutorial
+router.post('/', (req,res) => {
+    if (!req.body.title) {
+        res.status(400).send({ message: "Content can not be empty!" });
+        return;
+      }
+    
+      // Create a Tutorial
+      const tutorial = new Tutorial({
+        title: req.body.title,
+        description: req.body.description,
+        published: req.body.published ? req.body.published : false
+      });
+    
+      // Save Tutorial in the database
+      tutorial
+        .save(tutorial)
+        .then(data => {
+          res.send(data);
+        })
+        .catch(err => {
+          res.status(500).send({
+            message:
+              err.message || "Some error occurred while creating the Tutorial."
+          });
+        });
+})
+
+// getting a specific key , here title
+router.get('/:title', (req,res) => {
+    const title = req.params.title;
+    var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
+  
+    Tutorial.find(condition)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving tutorials."
+        });
+      });
+})
+
+//all published
+router.get('/published', (req,res) => {
+    Tutorial.find({ published: true })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials."
+      });
+    });
+})
+
+//a single tutorial with an id
+
+router.get('/:id', (req,res)=> {
+    const id = req.params.id;
+
+  Tutorial.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Tutorial with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Tutorial with id=" + id });
+    });
+})
+
+router.put('/:id', (req,res) => {
+    if (!req.body) {
+        return res.status(400).send({
+          message: "Data to update can not be empty!"
+        });
+      }
+    
+      const id = req.params.id;
+    
+      Tutorial.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+          if (!data) {
+            res.status(404).send({
+              message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found!`
+            });
+          } else res.send({ message: "Tutorial was updated successfully." });
+        })
+        .catch(err => {
+          res.status(500).send({
+            message: "Error updating Tutorial with id=" + id
+          });
+        });
+})
+
+router.delete('/:id', (req,res) => {
+    const id = req.params.id;
+
+  Tutorial.findByIdAndRemove(id)
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+        });
+      } else {
+        res.send({
+          message: "Tutorial was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Tutorial with id=" + id
+      });
+    });
+})
+
+router.delete('/', (req,res) => {
+    Tutorial.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Tutorials were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all tutorials."
+      });
+    });
+})
+
+module.exports = router
